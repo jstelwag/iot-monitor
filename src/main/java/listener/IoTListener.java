9595f42d1a46abcb66b9083bf8a1f74240d02401
Jaap Stelwagen <jaapstelwagen@gmail.com>
2016-09-25 09:01:36 +0200

    Restarts IoT listener when an exception occurs

--- src/main/java/listener/IoTListener.java
+++ src/main/java/listener/IoTListener.java
@@ -1,9 +1,7 @@
 package listener;
 
 import org.apache.commons.io.IOUtils;
-import org.apache.commons.lang3.SystemUtils;
 import speaker.LogstashLogger;
-import speaker.LogstashTimedSpeaker;
 
 import java.io.*;
 import java.net.ServerSocket;
@@ -13,11 +11,13 @@ import java.util.concurrent.*;
 public class IoTListener implements Runnable, Closeable {
     private final int WORKERS = 4;
 
-    final ServerSocket listener;
+    private ServerSocket listener;
     private final ThreadPoolExecutor executor;
+    private final int iotPort;
 
     public IoTListener(int iotPort) throws IOException {
         listener = new ServerSocket(iotPort);
+        this.iotPort = iotPort;
         // 2 threads active, max 4 threads before
         executor = new ThreadPoolExecutor(2, WORKERS, 0, TimeUnit.SECONDS,
                 new LinkedBlockingQueue<Runnable>()
@@ -33,12 +33,19 @@ public class IoTListener implements Runnable, Closeable {
             } catch (IOException e) {
                 LogstashLogger.INSTANCE.message("ERROR IoTListener connection failure, trying to continue "
                         + e.toString());
+                if (listener != null) {
+                    IOUtils.closeQuietly(listener);
+                }
+                try {
+                    listener = new ServerSocket(iotPort);
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
             }
         }
     }
 
     private void accept() throws IOException {
-
         Socket socket = listener.accept(); // waits for a connection
         if (executor.getActiveCount() == WORKERS) {
             LogstashLogger.INSTANCE.message("IoTListener rejecting connection");
