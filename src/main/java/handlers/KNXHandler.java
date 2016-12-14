95014193ce5733baa8cba6b2ca874e061d9c8313
Jaap Stelwagen <jaapstelwagen@gmail.com>
2016-12-14 10:02:05 +0100

    Moved to calimero master branche

--- src/main/java/handlers/KNXHandler.java
+++ src/main/java/handlers/KNXHandler.java
@@ -20,43 +20,68 @@ import java.util.regex.Pattern;
  */
 public class KNXHandler extends AbstractHandler {
 
-    public void run(String[] args) {
-        JSONObject knxResponse = process(args[1], new GroupAddress(Integer.parseInt(args[2]), Integer.parseInt(args[3]), Integer.parseInt(args[4])));
-        System.out.println(knxResponse.toString(4));
-    }
-
     @Override
     public void handle(String s, Request baseRequest, HttpServletRequest request, HttpServletResponse response)
             throws IOException, ServletException {
         System.out.println("KNX request " + s);
         response.setContentType("application/json");
-        Matcher matcher = null;
-        try {
-            Pattern pattern = Pattern.compile(Pattern.quote("/") + "(.*?)" + Pattern.quote("/") + "([0-9]+)"
-                    + Pattern.quote("/") + "([0-9]+)" + Pattern.quote("/") + "([0-9]+)" + Pattern.quote("/"));
-            matcher = pattern.matcher(s);
-        } catch (IllegalStateException e) {
-            response.getWriter().println("Match of " + s + " failed " + e.getMessage());
+        Pattern pattern = Pattern.compile(Pattern.quote("/") + "(.*?)" + Pattern.quote("/") + "(\\d+)"
+                + Pattern.quote("/") + "(\\d+)" + Pattern.quote("/") + "(\\d+)" + Pattern.quote("/") + ".*");
+        Matcher matcher = pattern.matcher(s);
+        JSONObject knxResponse = null;
+        if (matcher.find()) {
+            GroupAddress address = new GroupAddress(Integer.parseInt(matcher.group(2))
+                    , Integer.parseInt(matcher.group(3))
+                    , Integer.parseInt(matcher.group(4)));
+            switch (matcher.group(1)) {
+                case "float":
+                    knxResponse = getFloat(address);
+                    break;
+                case "writeFloat":
+                    Pattern pat = Pattern.compile(Pattern.quote("/") + "(.*?" + Pattern.quote("/") + "\\d+"
+                            + Pattern.quote("/") + "\\d+" + Pattern.quote("/") + "\\d+)" + Pattern.quote("/")
+                            + Pattern.quote("/") + "(([0-9]*\\.?[0-9]+)" + Pattern.quote("/"));
+                    Matcher mat = pat.matcher(s);
+                    if (mat.find()) {
+                        float soll = Float.parseFloat(mat.group(2));
+                        knxResponse = writeFloat(address, soll);
+                    }
+                    break;
+            }
         }
 
-        if (matcher != null) {
-            GroupAddress address = new GroupAddress(Integer.parseInt(matcher.group(1)), Integer.parseInt(matcher.group(2)), Integer.parseInt(matcher.group(3)));
-            JSONObject knxResponse = process(matcher.group(1), address);
+        if (knxResponse != null) {
             response.getWriter().println(knxResponse.toString(4));
         }
         response.setStatus(HttpServletResponse.SC_OK);
         baseRequest.setHandled(true);
     }
 
-    private JSONObject process(String action, GroupAddress address) {
+    private JSONObject getFloat(GroupAddress address) {
         JSONObject retVal = new JSONObject();
 
-        retVal.put("command", action);
+        retVal.put("command", "getFloat");
         retVal.put("group", address.toString());
         try {
             ProcessCommunicator pc = HeatingControl.INSTANCE.knxLink.pc();
             float value = pc.readFloat(address, false);
             retVal.put("knx return value", value);
+        } catch (KNXException | InterruptedException e) {
+            retVal.put("error", e.getMessage());
+        }
+
+        return retVal;
+    }
+
+    private JSONObject writeFloat(GroupAddress address, float soll) {
+        JSONObject retVal = new JSONObject();
+
+        retVal.put("command", "writeFloat");
+        retVal.put("group", address.toString());
+        retVal.put("value", soll);
+        try {
+            ProcessCommunicator pc = HeatingControl.INSTANCE.knxLink.pc();
+            pc.write(address, soll, true);
         } catch (KNXException | InterruptedException e) {
             retVal.put("error", e.getMessage());
         }
