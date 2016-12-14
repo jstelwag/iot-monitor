64041caaf99dbefb429656cb90838d40191c3739
Jaap Stelwagen <jaapstelwagen@gmail.com>
2016-12-14 20:22:39 +0100

    Added all theromostats

--- src/main/java/building/ControllableArea.java
+++ src/main/java/building/ControllableArea.java
@@ -4,9 +4,9 @@ import tuwien.auto.calimero.GroupAddress;
 
 public enum ControllableArea {
     apartment_I(Room.apartment_I, new GroupAddress(2, 5, 1), null, null, new GroupAddress(2, 7, 1), null, 1),
-    apartment_II(Room.apartment_II, new GroupAddress(1, 5, 1), new GroupAddress(1, 5, 6), null, new GroupAddress(1, 7, 0), null, 1),
+    apartment_II(Room.apartment_II, new GroupAddress(1, 5, 1), new GroupAddress(1, 5, 6), "1/5/8", new GroupAddress(1, 7, 0), "1/5/3", 1),
     apartment_II_bedroom(Room.apartment_II, new GroupAddress(1, 5, 61), null, null, null, null, 1),
-    apartment_III(Room.apartment_III, new GroupAddress(1, 5, 11), new GroupAddress(1, 5, 16), null, new GroupAddress(1, 7, 4), null, 1),
+    apartment_III(Room.apartment_III, new GroupAddress(1, 5, 11), new GroupAddress(1, 5, 16), "1/5/18", new GroupAddress(1, 7, 4), "1/5/13", 1),
     apartment_III_bathroom(Room.apartment_III, new GroupAddress(1, 5, 51), null, null, new GroupAddress(1, 7, 5), null, 1),
     room_a(Room.room_a, new GroupAddress(2, 5, 11), new GroupAddress(2, 5, 16), null, new GroupAddress(2, 7, 2), null, 1),
     room_b(Room.room_b, new GroupAddress(1, 5, 21), null, null, new GroupAddress(1, 7, 1), null, 1),
@@ -14,8 +14,8 @@ public enum ControllableArea {
     room_d(Room.room_d, new GroupAddress(1, 5, 41), null, null, new GroupAddress(1, 7, 6), null, 1),
     //Room 1 needs to bo colder so set the default mode to Away (2)
     room_1(Room.room_1, new GroupAddress(4, 5, 1), new GroupAddress(4, 5, 6), "4/5/6", new GroupAddress(4, 7, 0), "4/5/3", 2),
-    room_2_bathroom(Room.room_2, new GroupAddress(6, 5, 1), new GroupAddress(6, 5, 6), null, new GroupAddress(6, 7, 0), null, 1),
-    room_3_bathroom(Room.room_3, new GroupAddress(6, 5, 11), new GroupAddress(6, 5, 16), null, null, null, 1); //new GroupAddress(6,7,2) for this room
+    room_2_bathroom(Room.room_2, new GroupAddress(6, 5, 1), new GroupAddress(6, 5, 6), "6/5/8", new GroupAddress(6, 7, 0), "6/5/3", 1),
+    room_3_bathroom(Room.room_3, new GroupAddress(6, 5, 11), new GroupAddress(6, 5, 16), "6/5/18", null, "6/5/13", 1);
 
     ControllableArea(Room room, GroupAddress temperatureSensor, GroupAddress setpoint, String offset, GroupAddress allOffButton, String modusAddress, int defaultModus) {
         this.room = room;
