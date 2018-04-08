a1c068193f1c4f31855996826a4395a67f7d8be2
Jaap Stelwagen <jaapstelwagen@gmail.com>
2018-04-08 11:28:53 +0200

    App II controller is broken, disabled for now

--- src/main/java/building/ControllableArea.java
+++ src/main/java/building/ControllableArea.java
@@ -4,7 +4,8 @@ import tuwien.auto.calimero.GroupAddress;
 
 public enum ControllableArea {
     apartment_I(Room.apartment_I, new GroupAddress(2, 5, 1), null, null, new GroupAddress(2, 7, 1), null, 1),
-    apartment_II(Room.apartment_II, new GroupAddress(1, 5, 1), new GroupAddress(1, 5, 6), "1/5/8", new GroupAddress(1, 7, 0), "1/5/3", 1),
+    //oldappii new GroupAddress(1, 5, 1), new GroupAddress(1, 5, 6)
+    apartment_II(Room.apartment_II, null, null, "1/5/8", new GroupAddress(1, 7, 0), "1/5/3", 1),
     apartment_II_bedroom(Room.apartment_II, new GroupAddress(1, 5, 61), null, null, null, null, 1),
     room_a(Room.room_a, new GroupAddress(2, 5, 11), new GroupAddress(2, 5, 16), null, new GroupAddress(2, 7, 2), null, 1),
     room_b(Room.room_b, new GroupAddress(1, 5, 21), null, null, new GroupAddress(1, 7, 1), null, 1),
