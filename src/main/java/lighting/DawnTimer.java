580ab0042e8d4d5b661b556f2269d05ecef7361e
Jaap Stelwagen <jaap.stelwagen@newgreen.market>
2018-12-09 15:24:30 +0100

    Compilation error

--- src/main/java/lighting/DawnTimer.java
+++ src/main/java/lighting/DawnTimer.java
@@ -13,7 +13,7 @@ public class DawnTimer extends TimerTask {
     @Override
     public void run() {
         Jedis jedis = new Jedis("localhost");
-        if (!"OFF".equals(jedis.get("indoor.state") && new Sun().dawn(5.0))) {
+        if (!"OFF".equals(jedis.get("indoor.state")) && new Sun().dawn(5.0)) {
             LogstashLogger.INSTANCE.info("Switching dawn indoor - lights off");
             jedis.set("indoor.state", "OFF");
             try {
@@ -27,7 +27,7 @@ public class DawnTimer extends TimerTask {
             }
         }
 
-        if (!"OFF".equals(jedis.get("outdoor.state") && new Sun().dawn(0.0))) {
+        if (!"OFF".equals(jedis.get("outdoor.state")) && new Sun().dawn(0.0)) {
             LogstashLogger.INSTANCE.info("Switching dawn outdoor - lights off");
             jedis.set("outdoor.state", "OFF");
             try {
