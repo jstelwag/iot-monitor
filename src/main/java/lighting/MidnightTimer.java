038d435a82d2001806a0d82a400df29d649af5cb
Jaap Stelwagen <jaap.stelwagen@newgreen.market>
2018-12-09 21:55:39 +0100

    Midnight timer now using schedule

--- src/main/java/lighting/MidnightTimer.java
+++ src/main/java/lighting/MidnightTimer.java
@@ -5,23 +5,22 @@ import speaker.LogstashLogger;
 import tuwien.auto.calimero.GroupAddress;
 import tuwien.auto.calimero.KNXException;
 
-import java.util.Date;
-
 public class MidnightTimer implements Runnable {
 
-
-    public Date midnight() {
-        Long time = new Date().getTime();
-        return new Date(time - time % (24 * 60 * 60 * 1000));
-    }
     @Override
     public void run() {
         LogstashLogger.INSTANCE.info("Switching midnight - lights off");
 
         try {
-            //3/0/106	button	garden	yet_unknown	koetshuis, buitenlamp lindeboom
-            KNXLink.getInstance().writeBoolean(new GroupAddress("3/0/106"), false);
-            Thread.sleep(500);
+            Schedule schedule = new Schedule();
+            for (String address : schedule.outdoorToMidnight) {
+                KNXLink.getInstance().writeBoolean(new GroupAddress(address), true);
+                Thread.sleep(500);
+            }
+            for (String address : schedule.indoorToMidnight) {
+                KNXLink.getInstance().writeBoolean(new GroupAddress(address), true);
+                Thread.sleep(500);
+            }
         } catch (KNXException | InterruptedException e) {
             LogstashLogger.INSTANCE.error("Midnight time knx swithching problem " + e.getMessage());
         }
