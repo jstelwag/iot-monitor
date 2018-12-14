674866f6ab78e92882161ce1d591f505f6abc4fc
Jaap Stelwagen <jaap.stelwagen@newgreen.market>
2018-12-14 16:08:34 +0100

    Compile error

--- src/main/java/knx/ToggleAllListener.java
+++ src/main/java/knx/ToggleAllListener.java
@@ -20,25 +20,24 @@ public class ToggleAllListener implements NetworkLinkListener {
         try {
             String event = frameEvent.getFrame().toString();
             KNXAddress knx = addressList.findInString(event);
-            if (knx != null) {
-                if (knx.type == KNXAddress.Type.homeserver) {
-                    Jedis jedis = new Jedis("localhost");
+            if (knx != null && knx.type == KNXAddress.Type.homeserver) {
+                Jedis jedis = new Jedis("localhost");
 
-                    boolean desiredState = !"ON".equals(jedis.get(knx.room + ".all.state"));
-                    jedis.set(knx.room + ".all.state", desiredState ? "ON" : "OFF");
-                    int switchCount = 0;
-                    for (KNXAddress address : addressList.addressesByRoom(knx.room, KNXAddress.Type.button)) {
+                boolean desiredState = !"ON".equals(jedis.get(knx.room + ".all.state"));
+                jedis.set(knx.room + ".all.state", desiredState ? "ON" : "OFF");
+                int switchCount = 0;
+                for (KNXAddress address : addressList.addressesByRoom(knx.room, KNXAddress.Type.button)) {
                     //    try {
                     //        KNXLink.getInstance().writeBoolean(new GroupAddress(address.address), desiredState);
-                            switchCount++;
-                            //Thread.sleep(50); //Wait a little to reduce LED switching peaks
+                    switchCount++;
+                    //Thread.sleep(50); //Wait a little to reduce LED switching peaks
                     //    } catch (KNXException | InterruptedException e) {
                     //        LogstashLogger.INSTANCE.error("Failed to toggle room " + address + ", " + e.getMessage());
                     //    }
-                    }
-                    LogstashLogger.INSTANCE.info("Toggled room " + knx.room + ", switched " + switchCount
-                            + " lights " + (desiredState ? "on" : "off"));
                 }
+                LogstashLogger.INSTANCE.info("Toggled room " + knx.room + ", switched " + switchCount
+                        + " lights " + (desiredState ? "on" : "off"));
+            }
         } catch (Exception e) {
             LogstashLogger.INSTANCE.error("Caught unexpected exception, " + e.getMessage());
         }
