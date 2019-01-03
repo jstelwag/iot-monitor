f34fbe6fe6c587813085233f29fd05a526a4873e
Jaap Stelwagen <jaapstelwagen@gmail.com>
2019-01-03 22:21:47 +0100

    Refactored influxdb table names

--- src/main/java/util/LineProtocolUtil.java
+++ src/main/java/util/LineProtocolUtil.java
@@ -34,15 +34,16 @@ public class LineProtocolUtil {
 
     public static String protocolLine(HeatZone zone, String type, String value) {
         StringBuilder retVal = new StringBuilder();
+        retVal.append("valvegroup,name=\"").append(zone.group).append("\"");
+        retVal.append(",furnace=\"").append(zone.group.furnace).append("\"");
+        retVal.append(",sequence=").append(zone.groupSequence).append('i');
         if (zone.controllableArea != null) {
-            retVal.append(zone.controllableArea).append(",position=").append(zone.position);
-            if (zone.area != null) {
-                retVal.append(",area=").append(escape(zone.area));
-            }
-            retVal.append(",sequence=").append(zone.groupSequence);
-        } else {
-            retVal.append(zone.group).append(",sequence=").append(zone.groupSequence).append('i');
+            retVal.append(",room=\"").append(zone.controllableArea).append("\",position=").append(zone.position);
         }
+        if (zone.area != null) {
+                retVal.append(",area=\"").append(escape(zone.area)).append("\"");
+        }
+
         retVal.append(' ').append(type).append('=').append(value);
 
         return retVal.toString();
@@ -50,7 +51,9 @@ public class LineProtocolUtil {
 
     public static String protocolLine(HeatZone.ValveGroup group, int sequence, String type, String value) {
         StringBuilder retVal = new StringBuilder();
-        retVal.append(group).append(",sequence=").append(sequence).append('i');
+        retVal.append("valvegroup,name=\"");
+        retVal.append(group).append("\",sequence=").append(sequence).append('i');
+        retVal.append(",furnace=\"").append(group.furnace).append("\"");
         retVal.append(' ').append(type).append('=').append(value);
 
         return retVal.toString();
@@ -58,20 +61,20 @@ public class LineProtocolUtil {
 
     public static String protocolLine(ControllableArea controllableArea, String type, String value) {
         StringBuilder retVal = new StringBuilder();
-        retVal.append(controllableArea).append(",position=room");
+        retVal.append("room,name=\"").append(controllableArea).append("\"");
         retVal.append(' ').append(type).append('=').append(value);
 
         return retVal.toString();
     }
 
     public static String protocolLine(Room room, String customer) {
-        return "booking,room=" + room + " customer=\"" + customer.replace("\"", " ") +"\"";
+        return "booking,room=" + room + " customer=\"" + customer.replace("\"", " ") + "\"";
     }
-
 
     public static String protocolLine(Furnace furnace, String type, String value) {
         StringBuilder retVal = new StringBuilder();
-        retVal.append(furnace).append(' ').append(type).append('=').append(value);
+        retVal.append("furnace,name=\"");
+        retVal.append(furnace).append("\" ").append(type).append('=').append(value);
 
         return retVal.toString();
     }
